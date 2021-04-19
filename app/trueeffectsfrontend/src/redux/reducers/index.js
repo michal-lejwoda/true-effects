e2351905e76e1a8e4b150a1d5ef576c2d62bd225
saxatachi <saxatachi@gmail.com>
2021-04-19 09:20:23 +0200

    build

--- app/trueeffectsfrontend/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import trainingReducer from './trainingReducer';
-import authenticationReducer from './authenticationReducer';
-export default combineReducers({
-    training:trainingReducer,
-    authentication:authenticationReducer,
-    
-})
