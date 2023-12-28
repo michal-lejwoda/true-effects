6773150b217fbc9a2109d1eb26ddf9b12303b909
michal <m.lejwoda@sunshineprofits.com>
2023-12-28 08:27:47 +0100

    cleaning auth

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-# Create your models here.
+from rest_framework.authtoken.models import Token
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    @property
+    def return_dict_data_with_token(self):
+        token = Token.objects.get(user=self).key
+        return {'response': "Poprawnie zarejestrowano użytkownika", 'email': self.email, 'username': self.username,
+                'token': token}
+    @property
+    def return_login_dict_with_token(self):
+        token, created = Token.objects.get_or_create(user=self)
+        return {
+            'token': token.key,
+            'username': self.username,
+        }
