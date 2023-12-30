2f6a45d1bb4028b3227b6fae29df50684cf79cf8
michal <m.lejwoda@sunshineprofits.com>
2023-12-30 14:38:00 +0100

    some index changes

--- trueeffectsfrontend/src/indexv2.js
+++ trueeffectsfrontend/src/indexv2.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginContainer from './components/LoginContainer';
 import DefaultContainer from './components/DefaultContainer';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route,Switch } from 'react-router-dom';
 import {Provider} from 'react-redux'
 import store from './store';
@@ -22,4 +21,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals();
+
