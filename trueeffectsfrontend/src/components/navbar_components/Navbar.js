a41814c43f778cf12c342f4132f66fe4189f6dd1
michal <m.lejwoda@sunshineprofits.com>
2024-01-12 23:45:05 +0100

    Antoher css changes

--- trueeffectsfrontend/src/components/navbar_components/Navbar.js
+++ trueeffectsfrontend/src/components/navbar_components/Navbar.js
@@ -3,8 +3,12 @@ import React, {useState} from "react";
 import {faDumbbell, faWeight} from "@fortawesome/fontawesome-free-solid";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {handleMoveToCreateTraining} from "../helpers/history_helpers";
+import {useHistory} from "react-router-dom";
 const Navbar = () =>{
+    const history = useHistory()
     const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+    console.log(isMobileNavOpen)
 
     return (
         <nav className="nav_bar">
@@ -12,15 +16,15 @@ const Navbar = () =>{
             <div className="nav_bar__logo"><FontAwesomeIcon icon={faDumbbell}/>TrueEffects</div>
             <ul className="nav_bar__desktop">
                 <li className="nav_bar__element nav_bar__desktop__element"><a>Kalendarz treningów</a></li>
-                <li className="nav_bar__element nav_bar__desktop__element"><a>Kreator treningu</a></li>
+                <li className="nav_bar__element nav_bar__desktop__element" onClick={()=>handleMoveToCreateTraining(history)}><a>Kreator treningu</a></li>
                 <li className="nav_bar__element nav_bar__desktop__element"><a>Cele</a></li>
                 <li className="nav_bar__element nav_bar__desktop__element"><a>Pomiary</a></li>
                 <li className="nav_bar__element nav_bar__desktop__element"><a>Ustawienia</a></li>
             </ul>
             <ul className="nav_bar__mobile" style={{ display: isMobileNavOpen ? 'flex' : 'none' }}>
                 <li className="nav_bar__element nav_bar__mobile__element"><a>Kalendarz treningów</a></li>
-                <li className="nav_bar__element nav_bar__mobile__element"><a>Kreator treningu</a></li>
-                <li className="nav_bar__element nav_bar__mobile__element"><a>Cele</a></li>
+                <li className="nav_bar__element nav_bar__mobile__element" onClick={()=>handleMoveToCreateTraining(history)}><a>Kreator treningu</a></li>
+                <li className="nav_bar__element nav_bar__mobile__element" onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}><a>Cele</a></li>
                 <li className="nav_bar__element nav_bar__mobile__element"><a>Pomiary</a></li>
                 <li className="nav_bar__element nav_bar__mobile__element"><a>Ustawienia</a></li>
             </ul>
