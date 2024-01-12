57e929c2527aaedeb3be59130114222812504e18
michal <m.lejwoda@sunshineprofits.com>
2024-01-12 23:21:15 +0100

    css changes

--- trueeffectsfrontend/src/components/DefaultContainer.js
+++ trueeffectsfrontend/src/components/DefaultContainer.js
@@ -31,9 +31,10 @@ import {useAuth} from "./hooks";
 import Scheduler from "./main_components/Scheduler";
 import Trainingv2 from "./main_components/Trainingv2";
 import ModifyTraining from "./main_components/ModifyTraining";
-import { FormikProvider} from 'formik'
+import {FormikProvider} from 'formik'
 import ModifyTrainingv2 from "./main_components/ModifyTrainingv2";
 import CreateTrainingv2 from "./main_components/CreateTrainingv2";
+import Navbar from "./navbar_components/Navbar";
 
 const DefaultContainer = (props) => {
     const {} = useAuth(props.token, props.loadToken, props.postLogoutAuth, props.history)
@@ -53,13 +54,13 @@ const DefaultContainer = (props) => {
     }, [props.token])
 
     return (
-        <div className="containerdefault">
-            {/*{props.loadedtrainings && props.loadedgoals && props.loadedmeasurements && props.loadedexercises ? */}
-            <>
-                <UpperNavbar/>
-                <SideNavbar/>
+        <>
+            <Navbar />
+            {/*<UpperNavbar/>*/}
+            {/*<SideNavbar/>*/}
+            <div className="container_default">
+                {/*{props.loadedtrainings && props.loadedgoals && props.loadedmeasurements && props.loadedexercises ? */}
                 <Route exact path="/" component={Homepage}/>
-                {/*<Route path="/training" component={Training}/>*/}
                 <Route path="/schedule" component={Schedule}/>
                 <Route path="/addmeasurements" component={AddMeasurements}/>
                 <Route path="/displaymeasurements" component={DisplayMeasurements}/>
@@ -72,9 +73,10 @@ const DefaultContainer = (props) => {
                 <Route path="/training" component={Trainingv2}/>
                 <Route path="/modify_training" component={ModifyTrainingv2}/>
                 <Route path="/create_training" component={CreateTrainingv2}/>
-            </>
-            {/*// : <BoxLoading />}*/}
-        </div>
+
+                {/*// : <BoxLoading />}*/}
+            </div>
+        </>
     )
 }
 const mapStateToProps = (state) => {
--- trueeffectsfrontend/src/components/create_training_components/CreateMultiSeries.js
+++ trueeffectsfrontend/src/components/create_training_components/CreateMultiSeries.js
@@ -24,71 +24,69 @@ const CreateMultiSeries = (props) => {
     const [loadExercises, addToSingleSeries, addMultiSingleSeries, handleChangeExercise] = useCreateMultiSeries(props, values, setFieldValue, setErrors)
 
     return (
-        <div className="createtraining__bottom">
-            <form onSubmit={handleSubmit}>
-                <div className="createtraining__bottom__leftcontainer">
-                    <h1>Stwórz Serie</h1>
-                    <div className="createtraining__bottom__leftcontainer__fields__elements">
-                        <div className="createtraining__bottom__leftcontainer__fields__element">
-                            <label htmlFor="">Wybierz ćwiczenie</label>
-                            <AsyncSelect name="exercise" onChange={handleChangeExercise} loadOptions={loadExercises}
-                                         defaultOptions/>
-                            {errors.exercise && <p>{errors.exercise}</p>}
-                        </div>
-                        <div className="createtraining__bottom__leftcontainer__fields__element">
-                            <label htmlFor="">Dodatkowa waga</label>
-                            <input name="extra_weight" onChange={handleChange} value={values.extra_weight} type="text"/>
-                            {errors.extra_weight && <p>{errors.extra_weight}</p>}
-                        </div>
-                        <div className="createtraining__bottom__leftcontainer__fields__element">
-                            <label htmlFor="">Czas przerwy</label>
-                            <input name="rest" onChange={handleChange} value={values.rest} type="number"/>
-                            {errors.rest && <p>{errors.rest}</p>}
-                        </div>
-                        <div className="createtraining__bottom__leftcontainer__fields__element">
-                            <label htmlFor="">Liczba powtórzeń</label>
-                            <input name="reps" onChange={handleChange} value={values.reps} type="number"/>
-                            {errors.reps && <p>{errors.reps}</p>}
-                        </div>
-                        <div className="createtraining__bottom__leftcontainer__fields__element">
-                            <label htmlFor="">Faza koncentryczna</label>
-                            <input name='concentric_phase' onChange={handleChange} value={values.concentric_phase}
-                                   type="number"/>
-                            {errors.concentric_phase && <p>{errors.concentric_phase}</p>}
-                        </div>
-                        <div className="createtraining__bottom__leftcontainer__fields__element">
-                            <label htmlFor="">Pauza po fazie koncentrycznej</label>
-                            <input name="pause_after_concentric_phase" onChange={handleChange}
-                                   value={values.pause_after_concentric_phase} type="number"/>
-                            {errors.pause_after_concentric_phase && <p>{errors.pause_after_concentric_phase}</p>}
-                        </div>
-                        <div className="createtraining__bottom__leftcontainer__fields__element">
-                            <label htmlFor="">Faza ekscentryczna</label>
-                            <input name="eccentric_phase" onChange={handleChange} value={values.eccentric_phase}
-                                   type="number"/>
-                            {errors.eccentric_phase && <p>{errors.eccentric_phase}</p>}
-                        </div>
-                        <div className="createtraining__bottom__leftcontainer__fields__element">
-                            <label htmlFor="">Pauza po fazie ekscentrycznej</label>
-                            <input name="pause_after_eccentric_phase" onChange={handleChange}
-                                   value={values.pause_after_eccentric_phase} type="number"/>
-                            {errors.pause_after_eccentric_phase && <p>{errors.pause_after_eccentric_phase}</p>}
-                        </div>
+        <form onSubmit={handleSubmit}>
 
+                <h1>Stwórz Serie</h1>
+                <div className="createtraining__bottom__leftcontainer__fields__elements">
+                    <div className="createtraining__bottom__leftcontainer__fields__element">
+                        <label htmlFor="">Wybierz ćwiczenie</label>
+                        <AsyncSelect name="exercise" onChange={handleChangeExercise} loadOptions={loadExercises}
+                                     defaultOptions/>
+                        {errors.exercise && <p>{errors.exercise}</p>}
                     </div>
-                    <div className="createtraining__bottom__leftcontainer__buttons">
-                        <label htmlFor="">Liczba serii</label>
-                        <input name="series_count" onChange={handleChange} value={values.series_count} type="text"/>
-                        {errors.series_count && <p>{errors.series_count}</p>}
-                        <button onClick={addMultiSingleSeries} type="submit">Dodaj kilka serii</button>
-                        <button onClick={addToSingleSeries}>Dodaj pojedyńczą serie</button>
-                        <button type="submit">Zaakceptuj trening</button>
+                    <div className="createtraining__bottom__leftcontainer__fields__element">
+                        <label htmlFor="">Dodatkowa waga</label>
+                        <input name="extra_weight" onChange={handleChange} value={values.extra_weight} type="text"/>
+                        {errors.extra_weight && <p>{errors.extra_weight}</p>}
                     </div>
-
+                    <div className="createtraining__bottom__leftcontainer__fields__element">
+                        <label htmlFor="">Czas przerwy</label>
+                        <input name="rest" onChange={handleChange} value={values.rest} type="number"/>
+                        {errors.rest && <p>{errors.rest}</p>}
+                    </div>
+                    <div className="createtraining__bottom__leftcontainer__fields__element">
+                        <label htmlFor="">Liczba powtórzeń</label>
+                        <input name="reps" onChange={handleChange} value={values.reps} type="number"/>
+                        {errors.reps && <p>{errors.reps}</p>}
+                    </div>
+                    <div className="createtraining__bottom__leftcontainer__fields__element">
+                        <label htmlFor="">Faza koncentryczna</label>
+                        <input name='concentric_phase' onChange={handleChange} value={values.concentric_phase}
+                               type="number"/>
+                        {errors.concentric_phase && <p>{errors.concentric_phase}</p>}
+                    </div>
+                    <div className="createtraining__bottom__leftcontainer__fields__element">
+                        <label htmlFor="">Pauza po fazie koncentrycznej</label>
+                        <input name="pause_after_concentric_phase" onChange={handleChange}
+                               value={values.pause_after_concentric_phase} type="number"/>
+                        {errors.pause_after_concentric_phase && <p>{errors.pause_after_concentric_phase}</p>}
+                    </div>
+                    <div className="createtraining__bottom__leftcontainer__fields__element">
+                        <label htmlFor="">Faza ekscentryczna</label>
+                        <input name="eccentric_phase" onChange={handleChange} value={values.eccentric_phase}
+                               type="number"/>
+                        {errors.eccentric_phase && <p>{errors.eccentric_phase}</p>}
+                    </div>
+                    <div className="createtraining__bottom__leftcontainer__fields__element">
+                        <label htmlFor="">Pauza po fazie ekscentrycznej</label>
+                        <input name="pause_after_eccentric_phase" onChange={handleChange}
+                               value={values.pause_after_eccentric_phase} type="number"/>
+                        {errors.pause_after_eccentric_phase && <p>{errors.pause_after_eccentric_phase}</p>}
+                    </div>
 
                 </div>
-            </form>
-        </div>
+                <div className="createtraining__bottom__leftcontainer__buttons">
+                    <label htmlFor="">Liczba serii</label>
+                    <input name="series_count" onChange={handleChange} value={values.series_count} type="text"/>
+                    {errors.series_count && <p>{errors.series_count}</p>}
+                    <button onClick={addMultiSingleSeries} type="submit">Dodaj kilka serii</button>
+                    <button onClick={addToSingleSeries}>Dodaj pojedyńczą serie</button>
+                    <button type="submit">Zaakceptuj trening</button>
+                </div>
+
+
+
+        </form>
     );
 };
 
