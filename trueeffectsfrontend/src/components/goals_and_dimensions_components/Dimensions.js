2594b15828d75a8d17e85f67fa52f8343d5cfe70
michal <m.lejwoda@sunshineprofits.com>
2024-01-19 16:25:56 +0100

    Dimension scss

--- trueeffectsfrontend/src/components/goals_and_dimensions_components/Dimensions.js
+++ trueeffectsfrontend/src/components/goals_and_dimensions_components/Dimensions.js
@@ -17,49 +17,53 @@ const Dimensions = (props) => {
 
     return (
         <div className="dimensions">
-            <h1 className="title dimensions__title">Ostatni pomiar</h1>
-            <div className="dimensions__buttons">
-                <button className="standard-button dimensions__buttons__create" onClick={handleShowCreateDimension}>+
-                    Dodaj nowy pomiar
-                </button>
-                <button className="standard-button dimensions__buttons__compare" onClick={handleShowCompareDimensions}>+
-                    Porównaj Pomiary
-                </button>
-            </div>
-            {props.userDimensions.length > 0 &&
-                <div className="dimensions__date">
-                    <input disabled={true} value={props.userDimensions[0].date} type="text"/>
-                </div>}
+            <div className="dimensions--last-dimension">
+                <h1 className="title dimensions__title">Ostatni pomiar</h1>
+                <div className="dimensions__buttons">
+                    <button className="standard-button dimensions__buttons__create"
+                            onClick={handleShowCreateDimension}>+
+                        Dodaj nowy pomiar
+                    </button>
+                    <button className="standard-button dimensions__buttons__compare"
+                            onClick={handleShowCompareDimensions}>+
+                        Porównaj Pomiary
+                    </button>
+                </div>
+                {props.userDimensions.length > 0 &&
+                    <div className="dimensions__date">
+                        <input disabled={true} value={props.userDimensions[0].date} type="text"/>
+                    </div>}
 
-            {props.userDimensions.length > 0 ?
-                Object.keys(props.userDimensionConfigurationForCompare).map(element => {
-                    return (
-                        <div key={element}>
-                            <div className="animatedInput">
-                                <input
-                                    defaultValue={props.userDimensions[0][element]}
-                                    type="text" disabled="true"/>
-                                <span>{props.userDimensionConfigurationForCompare[element]}</span>
+                {props.userDimensions.length > 0 ?
+                    Object.keys(props.userDimensionConfigurationForCompare).map(element => {
+                        return (
+                            <div key={element}>
+                                <div className="animatedInput">
+                                    <input
+                                        defaultValue={props.userDimensions[0][element]}
+                                        type="text" disabled="true"/>
+                                    <span>{props.userDimensionConfigurationForCompare[element]}</span>
+                                </div>
                             </div>
-                        </div>
-                    )
-                }) : <p>Nie masz jeszcze żadnych pomiarów</p>}
-            <CreateDimension show={showCreateDimension} handleClose={handleCloseCreateDimension}
-                             handleShow={handleShowCreateDimension}
-                             getDimensions={props.getDimensions}
-                             userDimensionConfiguration={props.userDimensionConfiguration}
-                             userDimensionsForCreate={props.userDimensionsForCreate}
-                             userDimensionConfigurationForCompare={props.userDimensionConfigurationForCompare}
+                        )
+                    }) : <p>Nie masz jeszcze żadnych pomiarów</p>}
+                <CreateDimension show={showCreateDimension} handleClose={handleCloseCreateDimension}
+                                 handleShow={handleShowCreateDimension}
+                                 getDimensions={props.getDimensions}
+                                 userDimensionConfiguration={props.userDimensionConfiguration}
+                                 userDimensionsForCreate={props.userDimensionsForCreate}
+                                 userDimensionConfigurationForCompare={props.userDimensionConfigurationForCompare}
 
-                             userDimensions={props.userDimensions}
-                             postDimension={props.postDimension}
-            />
-            <CompareDimensions show={showCompareDimensions} handleClose={handleCloseCompareDimensions}
-                               handleShow={handleShowCompareDimensions}
-                               userDimensions={props.userDimensions}
-                               userDimensionConfiguration={props.userDimensionConfiguration}
-                               userDimensionConfigurationForCompare={props.userDimensionConfigurationForCompare}
-            />
+                                 userDimensions={props.userDimensions}
+                                 postDimension={props.postDimension}
+                />
+                <CompareDimensions show={showCompareDimensions} handleClose={handleCloseCompareDimensions}
+                                   handleShow={handleShowCompareDimensions}
+                                   userDimensions={props.userDimensions}
+                                   userDimensionConfiguration={props.userDimensionConfiguration}
+                                   userDimensionConfigurationForCompare={props.userDimensionConfigurationForCompare}
+                />
+            </div>
         </div>
     );
 };
