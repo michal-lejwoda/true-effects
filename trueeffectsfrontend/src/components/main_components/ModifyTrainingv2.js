2b48189db0869c1b93bebc5867737e2bdd37bd14
michal <m.lejwoda@sunshineprofits.com>
2024-01-20 19:20:58 +0100

    Css Modify Training

--- trueeffectsfrontend/src/components/main_components/ModifyTrainingv2.js
+++ trueeffectsfrontend/src/components/main_components/ModifyTrainingv2.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Field, Formik} from "formik";
 import {connect} from "react-redux";
 import {createTraining, deleteCurrentTraining, updateTraining} from "../../redux/actions/trainingActions";
@@ -10,6 +10,7 @@ import "../../new_sass/modify_training.scss"
 
 const ModifyTrainingv2 = (props) => {
     const history = useHistory()
+    const [visibleElements, setVisibleElements] = useState([]);
     const handleModifyTraining = async (data) => {
         console.log("handleModifyTraining")
         console.log(data)
@@ -24,26 +25,36 @@ const ModifyTrainingv2 = (props) => {
         await props.deleteCurrentTraining(id)
     }
     const handleDate = (date, values) => {
-        console.log("date")
-        console.log(date)
-        console.log("values")
-        console.log(values)
         values.date = date
 
     }
+
+
+    const toggleVisibility = (elementId) => {
+        setVisibleElements((prevVisibleElements) => {
+            if (prevVisibleElements.includes(elementId)) {
+                return prevVisibleElements.filter((id) => id !== elementId);
+            } else {
+                return [...prevVisibleElements, elementId];
+            }
+        });
+    };
+
     return (
         <div className="modify-training">
             <Formik
                 initialValues={props.training}
-                onSubmit={(values, {setSubmitting}) => {console.log("submit")}}>
+                onSubmit={(values, {setSubmitting}) => {
+                    console.log("submit")
+                }}>
                 {({
                       values,
                       setFieldValue,
                       handleChange,
                       handleSubmit,
                   }) => (
-                    <form onSubmit={handleSubmit}>
-                        <div>
+                    <form className="modify-training__form" onSubmit={handleSubmit}>
+                        <div className="mt-data modify-training__mt-data">
                             <h1 className="title modify-training__title">Modyfikuj Trening</h1>
                             <DatePicker locale='pl'
                                         name="date"
@@ -64,39 +75,55 @@ const ModifyTrainingv2 = (props) => {
                                        type="text"/>
                                 <span>Opis treningu</span>
                             </div>
-                            <div className="modify_training__container">
-                                <div className="modify_training__container__buttons">
-                                    <button className="standard-button" onClick={() => handleMoveToTraining(history)}>Trenuj -></button>
-                                    <button className="standard-button" onClick={() => handleDeleteTraining(values.id)}>Usuń trening -</button>
-                                    <button className="standard-button" onClick={() => handleCopyTrainingToAnotherDate(values)}>Dodaj trening do
+                            <div className="mt-data--list">
+                                <div className="mt-data__buttons">
+                                    <button className="standard-button"
+                                            onClick={() => handleMoveToTraining(history)}>Trenuj ->
+                                    </button>
+                                    <button className="standard-button"
+                                            onClick={() => handleDeleteTraining(values.id)}>Usuń trening -
+                                    </button>
+                                    <button className="standard-button"
+                                            onClick={() => handleCopyTrainingToAnotherDate(values)}>Dodaj trening do
                                         innego dnia +
                                     </button>
                                     <button className="standard-button" type="submit">Modyfikuj trening</button>
 
                                 </div>
-                                <div className="modify_training__container__multiseries">
+                                <div className="multiseries mt-data__multiseries">
                                     {values.multi_series.map((multiseries, index) => {
                                         return (
-                                            <div className="modify_training__container__multiseries__element">
-                                                <div
-                                                    className="modify_training__container__multiseries__element_container">
-                                                    <div
-                                                        className="modify_training__container__multiseries__element_container--name">{multiseries.exercise.name}</div>
-                                                    <div
-                                                        className="modify_training__container__multiseries__element_container--expand">+
-                                                    </div>
+                                            <div className="multi-element multiseries__multi-element--collapse">
+                                                <div className="multi-element__container">
+                                                    <div className="multi-element__name">{multiseries.exercise.name}</div>
+                                                    <button className="buttons__button multi-element__button"
+                                                            onClick={() => toggleVisibility(multiseries)}>
+                                                        Pokaż
+                                                    </button>
                                                 </div>
                                                 <div
-                                                    className="modify_training__container__multiseries__element__singleseries">
-                                                    {multiseries.single_series.map((singleseries, indexv2) => {
+                                                    className="single-series multi-element__single-series--expanded">
+                                                    {visibleElements.includes(multiseries) && multiseries.single_series.map((singleseries, indexv2) => {
                                                         return (
-                                                            <div>
-                                                                <div
-                                                                    className="modify_training__container__multiseries__element__singleseries__element">
-                                                                    {singleseries.exercise.name}
-                                                                </div>
-                                                                <div
-                                                                    className="modify_training__container__multiseries__element__singleseries__element__expanded">
+                                                            <div className="single-series__element">
+                                                                {/*<div*/}
+                                                                {/*    className="modify_training__container__multiseries__element__singleseries__element">*/}
+                                                                {/*    {singleseries.exercise.name}*/}
+                                                                {/*</div>*/}
+                                                                    <div className="animatedInput">
+                                                                        <Field onChange={handleChange}
+                                                                               name={`multi_series[${index}].single_series[${indexv2}].reps`}
+                                                                               value={singleseries.reps}
+                                                                               type="text"/>
+                                                                        <span>Liczba powtórzeń</span>
+                                                                    </div>
+                                                                    <div className="animatedInput">
+                                                                        <Field onChange={handleChange}
+                                                                               name={`multi_series[${index}].single_series[${indexv2}].rest`}
+                                                                               value={singleseries.rest}
+                                                                               type="text"/>
+                                                                        <span>Odpoczynek</span>
+                                                                    </div>
                                                                     <div className="animatedInput">
                                                                         <Field onChange={handleChange}
                                                                                name={`multi_series[${index}].single_series[${indexv2}].concentricphase`}
@@ -128,8 +155,9 @@ const ModifyTrainingv2 = (props) => {
                                                                             onChange={handleChange} type="text"/>
                                                                         <span>Pauza po fazie ekscentrycznej</span>
                                                                     </div>
-                                                                </div>
-                                                            </div>)
+
+                                                            </div>
+                                                        )
                                                     })}
                                                 </div>
                                             </div>
