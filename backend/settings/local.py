622c8d33f47c5697ee8fa1de0a820047ea45d4ce
michal <m.lejwoda@sunshineprofits.com>
2024-01-21 23:35:39 +0100

    python settings

---
+++ backend/settings/local.py
@@ -0,0 +1,21 @@
+import dj_database_url
+from decouple import config
+from base import *
+DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+ALLOWED_HOSTS = ['*']
+
+"""DATABASES"""
+
+DATABASES = {'default': dj_database_url.config(default=config('DATABASE_URL'))}
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
+"""STATICFILES"""
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
