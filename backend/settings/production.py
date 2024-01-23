86471d4527a7790aea2d6d199f7d846ae44cc3fb
michal <m.lejwoda@sunshineprofits.com>
2024-01-23 04:12:50 +0100

    config fix

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -1,6 +1,7 @@
 import dj_database_url
 from decouple import Config, RepositoryEnv
 from .base import *
+
 DEBUG = False
 config = Config(RepositoryEnv('.env_production'))
 SECRET_KEY = config.get('PRODUCTION_SECRET_KEY')
@@ -11,7 +12,6 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {'default': dj_database_url.config(default=config.get('PRODUCTION_DATABASE_URL'))}
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-
 """STATICFILES"""
 
 STATIC_URL = '/production-static/'
--- compose/production/trueeffects_backend/entrypoint.sh
+++ compose/production/trueeffects_backend/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python manage.py migrate
+python manage.py migrate --settings=backend.settings.production
 exec gunicorn backend.wsgi:application -b 0.0.0.0:8000
