26d8079d6b96560b29d50204493489fd74544d00
michal <m.lejwoda@sunshineprofits.com>
2024-01-26 15:36:21 +0100

    Fix navbar scss warnings

--- trueeffectsfrontend/src/components/navbar_components/Navbar.js
+++ trueeffectsfrontend/src/components/navbar_components/Navbar.js
@@ -28,32 +28,32 @@ const Navbar = () => {
             <div className="nav_bar__logo"><FontAwesomeIcon icon={faDumbbell}/>TrueEffects</div>
             <ul className="nav_bar__desktop">
                 <li className="nav_bar__element nav_bar__desktop__element"
-                    onClick={() => handleMoveToScheduler(history)}><a>Kalendarz treningów</a></li>
+                    onClick={() => handleMoveToScheduler(history)}><button className="nav_bar__button">Kalendarz treningów</button></li>
                 <li className="nav_bar__element nav_bar__desktop__element"
-                    onClick={() => handleMoveToCreateTraining(history)}><a>Kreator treningu</a></li>
+                    onClick={() => handleMoveToCreateTraining(history)}><button className="nav_bar__button">Kreator treningu</button></li>
                 <li className="nav_bar__element nav_bar__desktop__element" onClick={() => handleMovetoGoals(history)}>
-                    <a>Cele</a></li>
+                    <button className="nav_bar__button">Cele</button></li>
                 <li className="nav_bar__element nav_bar__desktop__element"
-                    onClick={() => handleMovetoDimensions(history)}><a>Pomiary</a></li>
+                    onClick={() => handleMovetoDimensions(history)}><button className="nav_bar__button">Pomiary</button></li>
                 <li className="nav_bar__element nav_bar__desktop__element"
-                    onClick={() => handleMovetoSettings(history)}><a>Ustawienia</a></li>
+                    onClick={() => handleMovetoSettings(history)}><button className="nav_bar__button">Ustawienia</button></li>
             </ul>
             <ul className="nav_bar__mobile" style={{display: isMobileNavOpen ? 'flex' : 'none'}}>
                 <li className="nav_bar__element nav_bar__mobile__element"
-                    onClick={() => handleMoveToMobileScheduler(history, isMobileNavOpen, setIsMobileNavOpen)}><a>Kalendarz
-                    treningów</a></li>
+                    onClick={() => handleMoveToMobileScheduler(history, isMobileNavOpen, setIsMobileNavOpen)}><button className="nav_bar__button">Kalendarz
+                    treningów</button></li>
                 <li className="nav_bar__element nav_bar__mobile__element"
-                    onClick={() => handleMoveToMobileCreateTraining(history, isMobileNavOpen, setIsMobileNavOpen)}><a>Kreator
-                    treningu</a></li>
+                    onClick={() => handleMoveToMobileCreateTraining(history, isMobileNavOpen, setIsMobileNavOpen)}><button className="nav_bar__button">Kreator
+                    treningu</button></li>
                 <li className="nav_bar__element nav_bar__mobile__element" onClick={() => {
                     handleMoveToMobileGoals(history, isMobileNavOpen, setIsMobileNavOpen)
-                }}><a>Cele</a></li>
+                }}><button className="nav_bar__button">Cele</button></li>
                 <li className="nav_bar__element nav_bar__mobile__element"
                     onClick={() => handleMovetoMobileDimensions(history, isMobileNavOpen, setIsMobileNavOpen)}>
-                    <a>Pomiary</a></li>
+                    <button className="nav_bar__button">Pomiary</button></li>
                 <li className="nav_bar__element nav_bar__mobile__element"
                     onClick={() => handleMoveToMobileSettings(history, isMobileNavOpen, setIsMobileNavOpen)}>
-                    <a>Ustawienia</a></li>
+                    <button className="nav_bar__button">Ustawienia</button></li>
             </ul>
         </nav>
     )
