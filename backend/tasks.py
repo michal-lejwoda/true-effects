3cc4716e22441433f0022c470cca1ef855753fe2
michal <m.lejwoda@sunshineprofits.com>
2024-02-07 21:24:05 +0100

    working

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,11 +1,25 @@
-from celery import shared_task
+# from celery import Celery
+# from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from rest_framework.authtoken.models import Token
+#
+# import os
+# from celery import Celery
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.local')
+# os.environ.setdefault('AUTH_USER_MODEL', 'authorization.CustomUser')
+# app = Celery('tasks', broker='redis://trueeffects_redis:6379/0')
+# # app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.autodiscover_tasks()
+from . import celery_app
+@celery_app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
+# from rest_framework.authtoken.models import Token
 
-
-@shared_task
+@celery_app.task(bind=True)
 def send_reset_password_to_mail(email: str):
     try:
         # Pobranie tokenu dla użytkownika o podanym adresie e-mail
