f31437d1affd84f6fb04249f3d4c9814a528279b
michal <m.lejwoda@sunshineprofits.com>
2024-02-07 21:24:05 +0100

    working

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,1 +1,12 @@
-from .celery import app
+from .celery import app as celery_app
+from django.conf import settings
+def initialize_django():
+    from django.apps import apps
+    if not apps.ready:
+        apps.populate(settings.INSTALLED_APPS)
+        from rest_framework.authtoken.models import Token
+    # Tutaj możesz wykonywać operacje związane z Token lub innymi elementami Django
+
+# Wywołanie funkcji initialize_django(), aby załadować Django, gdy moduł zostanie zaimportowany
+initialize_django()
+__all__ = ('celery_app',)
--- backend/celery.py
+++ backend/celery.py
@@ -1,10 +1,14 @@
+import os
 from celery import Celery
 
-
-import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.local')
 os.environ.setdefault('AUTH_USER_MODEL', 'authorization.CustomUser')
 app = Celery('tasks', broker='redis://trueeffects_redis:6379/0')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
 # from rest_framework.authtoken.models import Token
