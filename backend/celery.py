b2dec4484daca8018359cfeb10fcae76e803233e
michal <m.lejwoda@sunshineprofits.com>
2024-02-09 02:44:23 +0100

    fix reset password

--- backend/celery.py
+++ backend/celery.py
@@ -1,14 +1,5 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.local')
-os.environ.setdefault('AUTH_USER_MODEL', 'authorization.CustomUser')
-app = Celery('tasks', broker='redis://trueeffects_redis:6379/0')
-# app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery('tasks', broker=f"redis://{os.environ.get('REDIS_CONTAINER')}:6379/0")
 app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
-# from rest_framework.authtoken.models import Token
