a03d55c46a2cffb80de655252e17f40449169992
michal <m.lejwoda@sunshineprofits.com>
2024-02-11 15:11:52 +0100

    fix ports

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -12,7 +12,26 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {'default': dj_database_url.config(default=config.get('PRODUCTION_DATABASE_URL'))}
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': config.get('REDIS_URL'),  # Replace with your Redis server information
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
+
 """STATICFILES"""
 
 STATIC_URL = '/production-static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'production-static')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config.get('EMAIL_HOST_PASSWORD')
