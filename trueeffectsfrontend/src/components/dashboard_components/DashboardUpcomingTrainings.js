b1054bd64ecf02eb027b26ec5e3c4b3e3195acf7
michal <m.lejwoda@sunshineprofits.com>
2024-02-22 22:17:38 +0100

    Fix create Training

--- trueeffectsfrontend/src/components/dashboard_components/DashboardUpcomingTrainings.js
+++ trueeffectsfrontend/src/components/dashboard_components/DashboardUpcomingTrainings.js
@@ -19,6 +19,7 @@ const DashboardUpcomingTrainings = (props) => {
                         .then((res) => {
                             props.getUpcomingTrainings()
                             props.getLastCompletedTrainings()
+                            props.getTrainings()
                             handleMoveToTraining(history, res.id)
                         })
                 }
