e302c8e25b7ff81e7f0cf91cf22eb8d4be323b9b
michal <m.lejwoda@sunshineprofits.com>
2024-02-29 22:58:20 +0100

    Login and Register fix

--- trueeffectsfrontend/src/components/validation/validation.js
+++ trueeffectsfrontend/src/components/validation/validation.js
@@ -72,8 +72,8 @@ export const createTrainingValidation = yup.object().shape({
 })
 
 export const loginUserValidation = yup.object().shape({
-    username: yup.string().min(2, "Nazwa użytkownika jest za krótka").max(100, "Nazwa użytkownika jest za długa ").required('Pole username jest wymagane'),
-    password: yup.string().min(2, "Hasło jest za krótkie").max(100, "Hasło jest za długie").required('Pole hasło jest wymagane'),
+    username: yup.string().min(4, "Nazwa użytkownika jest za krótka").max(100, "Nazwa użytkownika jest za długa ").required('Pole username jest wymagane'),
+    password: yup.string().min(4, "Hasło jest za krótkie").max(100, "Hasło jest za długie").required('Pole hasło jest wymagane'),
 })
 
 export const registerUserValidation = yup.object().shape({
