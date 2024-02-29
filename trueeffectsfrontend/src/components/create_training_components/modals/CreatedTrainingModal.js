468a5209120f7346dcd0b82f9450b6013330baab
michal <m.lejwoda@sunshineprofits.com>
2024-02-29 15:29:46 +0100

    CreateTraining changes

--- trueeffectsfrontend/src/components/create_training_components/modals/CreatedTrainingModal.js
+++ trueeffectsfrontend/src/components/create_training_components/modals/CreatedTrainingModal.js
@@ -2,8 +2,30 @@ import React from 'react';
 import Modal from "react-bootstrap/Modal";
 import {handleMoveToModifyTraining, handleMoveToScheduler, handleMoveToTraining} from "../../helpers/history_helpers";
 import {CloseButton} from "react-bootstrap";
+import {useHistory} from "react-router-dom";
 
 const CreatedTrainingModal = (props) => {
+    const history = useHistory()
+    const handleGoToTraining = (id) => {
+        props.getSingleTraining(id)
+            .then((training) => {
+                let date = new Date()
+                let res = date.toISOString().split('T')[0]
+                if (training.date === res) {
+                    handleMoveToTraining(history, id)
+                } else {
+                    let data = training
+                    data.date = res
+                    props.createTraining(data)
+                        .then((res) => {
+                            props.getUpcomingTrainings()
+                            props.getLastCompletedTrainings()
+                            props.getTrainings()
+                            handleMoveToTraining(history, res.id)
+                        })
+                }
+            })
+    }
     return (
         <div className="created-training-modal create-training__created-training-modal">
             <Modal show={props.showCreatedTrainingModal} onHide={props.handleCloseCreatedTrainingModal}>
@@ -19,7 +41,8 @@ const CreatedTrainingModal = (props) => {
                     <button className="standard-button"
                             onClick={() => handleMoveToModifyTraining(props.history, props.createdTraining.id)}>Modyfikuj trening
                     </button>
-                    <button className="standard-button" onClick={() => handleMoveToTraining(props.history, props.createdTraining.id)}>Ćwicz
+                    <button className="standard-button"
+                            onClick={() => handleGoToTraining(props.createdTraining.id)}>Ćwicz
                     </button>
                     <button className="standard-button" onClick={() => handleMoveToScheduler(props.history)}>Przejdź do
                         kalendarza
