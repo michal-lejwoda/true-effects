fb5eb5eb65f44a3e2d082f9a6718fcea424eb02d
michal <michal.lejwoda@gmail.com>
2024-07-10 07:14:29 +0200

    add user achievments model

--- achievements/models.py
+++ achievements/models.py
@@ -1,16 +1,20 @@
 from django.db import models
-
 from authorization.models import CustomUser
 
 
-# Create your models here.
 class Achievement(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    is_earned = models.BooleanField(default=False)
     image = models.ImageField(upload_to="achievements/image/")
+
+    def __str__(self):
+        return self.name
+
+class UserAchievement(models.Model):
+    achievement = models.ForeignKey(Achievement,  null=True, on_delete=models.SET_NULL)
+    is_earned = models.BooleanField(default=False)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='achievements')
     date_earned = models.DateTimeField(null=True, default=None)
 
     def __str__(self):
-        return self.name
+        return self.achievement.name
