a0daa8d5173f96d637616fb2268448bfea1ac9c8
michal <michal.lejwoda@gmail.com>
2024-08-25 00:50:37 +0200

    More translation change

--- trueeffectsfrontend/src/components/settings_components/SettingsPasswordItems.js
+++ trueeffectsfrontend/src/components/settings_components/SettingsPasswordItems.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {useFormik} from "formik";
 import {changePasswordValidation} from "../validation/validation";
+import {useTranslation} from "react-i18next";
 
 export const SettingsPasswordItems = (props) => {
+    const {t} = useTranslation();
     const {values, handleSubmit, handleChange, setErrors, errors} = useFormik({
         initialValues: {
             old_password: "", new_password1: "", new_password2: "",
@@ -23,27 +25,27 @@ export const SettingsPasswordItems = (props) => {
 
     return (
         <form onSubmit={handleSubmit}>
-            <h2 className="settings__title">Zmiana hasła</h2>
+            <h2 className="settings__title">{t("Password change")}</h2>
             <div className="animatedInput">
                 <input name="old_password" onChange={handleChange} value={values.old_password} required={true}
                        type="password"/>
-                <span>Stare hasło</span>
+                <span>{t('Old password')}</span>
             </div>
-            {errors.old_password && <p className="settings__errors--red">{errors.old_password}</p>}
+            {errors.old_password && <p className="settings__errors--red">{t(errors.old_password)}</p>}
             <div className="animatedInput">
                 <input name="new_password1" onChange={handleChange} value={values.new_password1} required={true}
                        type="password"/>
-                <span>Nowe hasło</span>
+                <span>{t('New password')}</span>
             </div>
-            {errors.new_password1 && <p className="settings__errors--red">{errors.new_password1}</p>}
+            {errors.new_password1 && <p className="settings__errors--red">{t(errors.new_password1)}</p>}
             <div className="animatedInput">
                 <input name="new_password2" onChange={handleChange} value={values.new_password2} required={true}
                        type="password"/>
-                <span>Powtórz nowe hasło</span>
+                <span>{t('Repeat new password')}</span>
             </div>
-            {errors.new_password2 && <p className="settings__errors--red">{errors.new_password2}</p>}
+            {errors.new_password2 && <p className="settings__errors--red">{t(errors.new_password2)}</p>}
             <div className="settings__accept-button">
-                <button className="standard-button" type="submit">Zapisz ustawienia</button>
+                <button className="standard-button" type="submit">{t('Save settings')}</button>
             </div>
         </form>
     );
