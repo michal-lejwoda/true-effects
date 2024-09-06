38dcab182f0460112f44801522df264846c0f582
michal <michal.lejwoda@gmail.com>
2024-09-06 13:27:07 +0200

    docker-compose -f local.yml run trueeffects_backend python manage.py test

--- authorization/tests.py
+++ authorization/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def test_correct_comparision(self):
+        self.assertEqual(1, 1)
+
+    def test_incorrect_comparision(self):
+        self.assertEqual(2, 1)
