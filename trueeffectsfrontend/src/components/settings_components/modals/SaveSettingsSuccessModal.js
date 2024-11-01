6881ac45977009063e80a6ca41f021c4e4eeae68
michal <michal.lejwoda@gmail.com>
2024-11-01 21:10:46 +0100

    add translation in SaveSettingsModal

--- trueeffectsfrontend/src/components/settings_components/modals/SaveSettingsSuccessModal.js
+++ trueeffectsfrontend/src/components/settings_components/modals/SaveSettingsSuccessModal.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import Modal from "react-bootstrap/Modal";
 import {CloseButton} from "react-bootstrap";
+import {t} from "i18next";
 
 const SaveSettingsSuccessModal = (props) => {
     return (
         <Modal className="transparent-modal" show={props.show} onHide={props.handleClose}>
             <Modal.Header>
-                <Modal.Title>Zapisano ustawienia</Modal.Title>
+                <Modal.Title>{t("Settings saved")}</Modal.Title>
                 <CloseButton onClick={props.handleClose} variant="white"/>
             </Modal.Header>
             <Modal.Body>
-                <p>Udało się zapisać ustawienia !!!</p>
+                <p>{t("Successfully saved the settings !!!")}</p>
             </Modal.Body>
             <Modal.Footer>
-                <button className="standard-button" onClick={props.handleClose}>Zamknij okno</button>
+                <button className="standard-button" onClick={props.handleClose}>{t("Close")}</button>
             </Modal.Footer>
         </Modal>
     );
