0029421fdae7686d86ccd867cee9e77c83325b0e
michal <michal.lejwoda@gmail.com>
2025-04-22 12:15:15 +0200

    Add vnet and subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,9 @@ module "resource-group" {
     resource_group_name = var.resource_group_name
     location = var.location
 }
+
+module "vnet" {
+    source = "./modules/vnet"
+    location = var.location
+    resource_group_name = var.resource_group_name
+}
