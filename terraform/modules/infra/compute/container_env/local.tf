4b21c14759eea2ab4b3b528370d58c80258c80d8
michal <michal.lejwoda@gmail.com>
2025-05-04 23:25:14 +0200

    Fix username and password

--- terraform/modules/infra/compute/container_env/local.tf
+++ terraform/modules/infra/compute/container_env/local.tf
@@ -1,5 +1,5 @@
 locals {
-  db_url = "postgres://${data.azurerm_key_vault_secret.db_username.value}:${data.azurerm_key_vault_secret.db_password.value}@${var.db_fqdn}:${data.azurerm_key_vault_secret.db_port.value}/${data.azurerm_key_vault_secret.db_name.value}"
+  db_url = "postgres://${data.azurerm_key_vault_secret.db_admin_username.value}:${data.azurerm_key_vault_secret.db_admin_password.value}@${var.db_fqdn}:${data.azurerm_key_vault_secret.db_port.value}/${data.azurerm_key_vault_secret.db_name.value}"
   redis_url = "rediss://${var.redis_primary_key}@${var.redis_hostname}:6380/0"
 }
 
