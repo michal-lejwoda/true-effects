4b9cebc46c954b82b9a7fbb385c762aec435dd1a
michal <michal.lejwoda@gmail.com>
2025-08-04 15:30:03 +0200

    Add providers

---
+++ terraform/environments/development/infra/deploy/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.38.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "3.4.0"
+    }
+
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+provider "azuread" {}
