0b252864030611d8dfc7f90cea2ed8f0435cdb74
michal <michal.lejwoda@gmail.com>
2025-08-06 22:31:00 +0200

    Change sku

--- terraform/environments/development/infra/deploy/database.tf
+++ terraform/environments/development/infra/deploy/database.tf
@@ -7,7 +7,7 @@ resource "azurerm_postgresql_flexible_server" "db" {
   # administrator_login    = var.db_admin_username
   administrator_password = "importantpassword"
   # administrator_password = var.db_admin_password
-  sku_name               = "B_Standard_B1ms"
+  sku_name               = "GP_Standard_D2s_v3"
   storage_mb             = 32768
   backup_retention_days  = 7
   delegated_subnet_id    = azurerm_subnet.private_a.id
@@ -58,13 +58,13 @@ resource "azurerm_private_dns_zone_virtual_network_link" "db_dns_zone_link" {
   virtual_network_id    = azurerm_virtual_network.main.id
 }
 
-# resource "azurerm_private_dns_a_record" "private_db_dns_a_record" {
-#   name                = "private_db_dns_a_record"
-#   zone_name           = azurerm_private_dns_zone.db_dns_zone.name
-#   resource_group_name = "true-effects-rgp"
-#   ttl                 = 300
-#   records             = [azurerm_private_endpoint.db_endpoint_a.private_service_connection[0].private_ip_address,
-#                          azurerm_private_endpoint.db_endpoint_b.private_service_connection[0].private_ip_address]
-# }
+resource "azurerm_private_dns_a_record" "private_db_dns_a_record" {
+  name                = "private_db_dns_a_record"
+  zone_name           = azurerm_private_dns_zone.db_dns_zone.name
+  resource_group_name = "true-effects-rgp"
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.db_endpoint_a.private_service_connection[0].private_ip_address,
+                         azurerm_private_endpoint.db_endpoint_b.private_service_connection[0].private_ip_address]
+}
 
 
