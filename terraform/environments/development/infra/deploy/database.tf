e09ddcd694cfde1381234bd34ac3932d275b3a9c
michal <michal.lejwoda@gmail.com>
2025-08-06 18:07:59 +0200

    another change

--- terraform/environments/development/infra/deploy/database.tf
+++ terraform/environments/development/infra/deploy/database.tf
@@ -10,40 +10,40 @@ resource "azurerm_postgresql_flexible_server" "db" {
   sku_name               = "B_Standard_B1ms"
   storage_mb             = 32768
   backup_retention_days  = 7
-  # delegated_subnet_id    = azurerm_subnet.private_a.id
-  # private_dns_zone_id    = azurerm_private_dns_zone.db_dns_zone.id
+  delegated_subnet_id    = azurerm_subnet.private_a.id
+  private_dns_zone_id    = azurerm_private_dns_zone.db_dns_zone.id
   public_network_access_enabled = false
   # zone                   = "1"
 }
 
 
-# resource "azurerm_private_endpoint" "db_endpoint_a" {
-#   name                = "db_endpoint"
-#   location            = "West Europe"
-#   resource_group_name = "true-effects-rgp"
-#   subnet_id           = azurerm_subnet.private_a.id
-#
-#   private_service_connection {
-#     name                           = "db-privatesc"
-#     private_connection_resource_id = azurerm_postgresql_flexible_server.db.id
-#     is_manual_connection           = false
-#     subresource_names              = ["postgresqlServer"]
-#   }
-# }
-#
-# resource "azurerm_private_endpoint" "db_endpoint_b" {
-#   name                = "db_endpoint_b"
-#   location            = "West Europe"
-#   resource_group_name = "true-effects-rgp"
-#   subnet_id           = azurerm_subnet.private_b.id
-#
-#   private_service_connection {
-#     name                           = "db-privatesc-b"
-#     private_connection_resource_id = azurerm_postgresql_flexible_server.db.id
-#     is_manual_connection           = false
-#     subresource_names              = ["postgresqlServer"]
-#   }
-# }
+resource "azurerm_private_endpoint" "db_endpoint_a" {
+  name                = "db_endpoint"
+  location            = "West Europe"
+  resource_group_name = "true-effects-rgp"
+  subnet_id           = azurerm_subnet.private_a.id
+
+  private_service_connection {
+    name                           = "db-privatesc"
+    private_connection_resource_id = azurerm_postgresql_flexible_server.db.id
+    is_manual_connection           = false
+    subresource_names              = ["postgresqlServer"]
+  }
+}
+
+resource "azurerm_private_endpoint" "db_endpoint_b" {
+  name                = "db_endpoint_b"
+  location            = "West Europe"
+  resource_group_name = "true-effects-rgp"
+  subnet_id           = azurerm_subnet.private_b.id
+
+  private_service_connection {
+    name                           = "db-privatesc-b"
+    private_connection_resource_id = azurerm_postgresql_flexible_server.db.id
+    is_manual_connection           = false
+    subresource_names              = ["postgresqlServer"]
+  }
+}
 
 
 resource "azurerm_private_dns_zone" "db_dns_zone" {
@@ -66,5 +66,5 @@ resource "azurerm_private_dns_zone_virtual_network_link" "db_dns_zone_link" {
 #   records             = [azurerm_private_endpoint.db_endpoint_a.private_service_connection[0].private_ip_address,
 #                          azurerm_private_endpoint.db_endpoint_b.private_service_connection[0].private_ip_address]
 # }
-#
+
 
