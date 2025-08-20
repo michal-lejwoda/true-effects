26e128d54d0ecb97c80f3bf56d91d074b65d59bc
michal <michal.lejwoda@gmail.com>
2025-08-20 18:34:52 +0200

    Add translation

--- trueeffectsfrontend/src/components/achievement_modals/AchievementNotification.js
+++ trueeffectsfrontend/src/components/achievement_modals/AchievementNotification.js
@@ -2,14 +2,15 @@ import React from 'react';
 import '../../new_sass/notification.scss'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faTimes, faTrophy} from "@fortawesome/free-solid-svg-icons";
+import {useTranslation} from "react-i18next";
 
 const AchievementNotification = ({message, onClose}) => {
-
+    const {t} = useTranslation();
     return (
         <div className="notification">
             <div className="notification__icon"><FontAwesomeIcon onClick={onClose} icon={faTimes}/></div>
 
-            <span className="notification__message"><FontAwesomeIcon onClick={onClose} icon={faTrophy}/>{message}</span>
+            <span className="notification__message"><FontAwesomeIcon onClick={onClose} icon={faTrophy}/>{t(message)}</span>
             {/*<button onClick={onClose}>Close</button>*/}
         </div>
     );
