724c2b0ed089068e7d30898357ddd63388723d51
michal <michal.lejwoda@gmail.com>
2025-08-20 16:37:39 +0200

    Fix notifications

--- trueeffectsfrontend/src/components/achievement_modals/AchievementNotification.js
+++ trueeffectsfrontend/src/components/achievement_modals/AchievementNotification.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../../new_sass/notification.scss'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faWindowClose} from "@fortawesome/free-solid-svg-icons";
+import {faTimes, faTrophy} from "@fortawesome/free-solid-svg-icons";
 
 const AchievementNotification = ({message, onClose}) => {
 
     return (
         <div className="notification">
-            <div className="notification__icon"><FontAwesomeIcon onClick={onClose} icon={faWindowClose}/></div>
-            <span className="notification__message">{message}</span>
+            <div className="notification__icon"><FontAwesomeIcon onClick={onClose} icon={faTimes}/></div>
+
+            <span className="notification__message"><FontAwesomeIcon onClick={onClose} icon={faTrophy}/>{message}</span>
             {/*<button onClick={onClose}>Close</button>*/}
         </div>
     );
